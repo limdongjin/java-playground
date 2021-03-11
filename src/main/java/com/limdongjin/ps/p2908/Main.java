166b569cf95895d75354edbe02b5da8534ab09f4
imdongjin <geniuslim27@gmail.com>
2021-03-12 00:43:05 +0900

    very easy boj problems

---
+++ src/main/java/com/limdongjin/ps/p2908/Main.java
@@ -0,0 +1,17 @@
+package com.limdongjin.ps.p2908;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// [TODO] goo
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+
+    }
+}
