be0af669fc3cdc9c0b69ead41d9d7cb41814cddf
imdongjin <geniuslim27@gmail.com>
2021-03-14 11:09:09 +0900

    [boj] 10845 큐
    LinkedList 를 Deque 으로 이용해서 해결

---
+++ src/main/java/com/limdongjin/ps/s/p10866/Main.java
@@ -0,0 +1,64 @@
+package com.limdongjin.ps.s.p10866;
+
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Deque<String> queue = new LinkedList<>();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String operation = st.nextToken();
+            switch (operation){
+                case "push_back":
+                    queue.addLast(st.nextToken());
+                    break;
+                case "push_front":
+                    queue.addFirst(st.nextToken());
+                    break;
+                case "front":
+                    if(queue.isEmpty())
+                        bw.write("-1\n");
+                    else
+                        bw.write(queue.getFirst()+"\n");
+                    break;
+                case "empty":
+                    if(queue.isEmpty())
+                        bw.write("1\n");
+                    else
+                        bw.write("0\n");
+                    break;
+                case "pop_back":
+                    if(queue.isEmpty())
+                        bw.write("-1\n");
+                    else
+                        bw.write(queue.removeLast()+"\n");
+                    break;
+                case "pop_front":
+                    if(queue.isEmpty())
+                        bw.write("-1\n");
+                    else
+                        bw.write(queue.removeFirst()+"\n");
+                    break;
+                case "back":
+                    if(queue.isEmpty())
+                        bw.write("-1\n");
+                    else
+                        bw.write(queue.getLast()+"\n");
+                    break;
+                case "size":
+                    bw.write(queue.size() + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
