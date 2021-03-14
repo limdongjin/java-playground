29512b5f64684c2b5a18a8c3af8cbd691d23eb90
imdongjin <geniuslim27@gmail.com>
2021-03-14 20:34:52 +0900

    [BOJ] 1929 소수 구하기
    에라토스체네스의 체

---
+++ src/main/java/com/limdongjin/ps/s/p1929/Main.java
@@ -0,0 +1,37 @@
+package com.limdongjin.ps.s.p1929;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 에라토스네스의 체
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int startInclusive = Integer.parseInt(st.nextToken());
+        int endInclusive = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrimeNumber = new boolean[endInclusive + 10];
+        Arrays.fill(isPrimeNumber, true);
+        isPrimeNumber[0] = false;
+        isPrimeNumber[1] = false;
+        for (int i = 2; i <= endInclusive; i++) {
+            if(!isPrimeNumber[i]) continue;
+            if(i >= startInclusive) bw.write(i+"\n");
+            for (int j = i + i; j <= endInclusive; j += i) {
+                isPrimeNumber[j] = false;
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
