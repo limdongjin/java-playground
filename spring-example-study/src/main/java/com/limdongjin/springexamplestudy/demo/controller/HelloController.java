1cc92cf4df8216520d8c5ccaeef8c78770cc8174
imdongjin <geniuslim27@gmail.com>
2021-03-18 00:08:26 +0900

    [spring-example-study] 템플릿 엔진 작동 로직 및 rl쿼리 핸들링
    url 쿼리
    - @RequestParam("your-parameter-name")
    
    템플릿 엔진 작동 로직
    1. '/hello-mvc?name=go' request
    2. 내장 톰캣 서버가 요청을 받고 이를 스프링 컨테이너에게 전달
    3. 해당 url에 매핑된 메서드를 호출한다. 호출시에 argument로 url 쿼리를 전달
    4. 해당 메서드는 작성된 로직을 수행하고, 'hello-mvc' 를 리턴
    5. ViewResolver 는 'hello-mvc' 템플릿 파일을 찾고, Thymeleaf 에게 처리 요청

--- spring-example-study/src/main/java/com/limdongjin/springexamplestudy/demo/controller/HelloController.java
+++ spring-example-study/src/main/java/com/limdongjin/springexamplestudy/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.limdongjin.springexamplestudy.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +13,12 @@ public class HelloController {
         model.addAttribute("data", "hello world!!!");
 
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name , Model model){
+        model.addAttribute("name", name);
+
+        return "hello-mvc";
     }
 }
