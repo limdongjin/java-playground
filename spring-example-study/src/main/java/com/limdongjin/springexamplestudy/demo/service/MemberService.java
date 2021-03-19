2c833c982eb438e78870477d603f6678db9a0c53
imdongjin <geniuslim27@gmail.com>
2021-03-19 21:16:48 +0900

    [spring-example-study] MemberService
    - tip: test 메서드에 주석으로 given, when, then 을 표시해놓으면 유용하다

---
+++ spring-example-study/src/main/java/com/limdongjin/springexamplestudy/demo/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.limdongjin.springexamplestudy.demo.service;
+
+import com.limdongjin.springexamplestudy.demo.domain.Member;
+import com.limdongjin.springexamplestudy.demo.repository.MemberRepository;
+import com.limdongjin.springexamplestudy.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+     * 회원 가입
+     */
+    public Long join(Member member) throws IllegalStateException{
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    /*
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName())
+                    .ifPresent(m -> {
+                        throw new IllegalStateException("이미 존재하는 회원 입니다.");
+                    });
+    }
+}
