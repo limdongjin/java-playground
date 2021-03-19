eaeb8ccdd0ba4dfb3d539e6fb778557fc381b31e
imdongjin <geniuslim27@gmail.com>
2021-03-19 23:25:29 +0900

    [spring-example-study] 간단한 회원가입, 회원 조회
    - thymeleaf 도 rails 의 erb 등의 템플릿 엔진과 거의 유사한 문법으로 코드 작성 가능

--- spring-example-study/src/main/java/com/limdongjin/springexamplestudy/demo/controller/MemberController.java
+++ spring-example-study/src/main/java/com/limdongjin/springexamplestudy/demo/controller/MemberController.java
@@ -3,8 +3,12 @@ package com.limdongjin.springexamplestudy.demo.controller;
 import com.limdongjin.springexamplestudy.demo.domain.Member;
 import com.limdongjin.springexamplestudy.demo.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -16,8 +20,32 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    @ResponseBody
-    public String getMembers(){
-        return memberService.findMembers().toString();
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+
+        // 1. attribute 이름 직접 설정
+        // model.addAttributes("members", members);
+
+        //2. attribute 이름을 conventional 하게 자동으로 설정해줌.
+        // 이 경우에는 memberlist 가 된다.
+        model.addAttribute(members);
+
+        return "members/memberList";
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
 }
