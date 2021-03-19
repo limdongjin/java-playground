ddc0c92b5be5466c2d56ba97a745f82cd618791e
imdongjin <geniuslim27@gmail.com>
2021-03-19 19:49:59 +0900

    [spring-example-study]json api 로 응답하기. HttpMessageConverter
    - @ResponseBody 어노테이션이 붙어있는 메서드라면 HttpMessageConverter 의 구현체가 처리를 하게된다.
    - 해당 어노테이션이 붙어있으면서 문자열을 반환하는 메서드는 기본적으로 StringHttpMessageConverter 가 처리를 한다.
    - 해당 어노테이션 붙어있으면서 객체를 리턴하는 메서드라면 기본적으로 MappingJackson2HttpMessageConverter 가 처리를 한다. 객체를 json 형식으로 바꿔서 반환하게 된다.
    - 주로 json 으로 반환하는 형식을 많이 사용한다.
    
    - 클라이언트의 HTTP Accept 헤더나 서버의 컨트롤러 반환타입 정보에 따라서 HttpMessage 컨트롤러가 선택되게 할수있다

--- spring-example-study/src/main/java/com/limdongjin/springexamplestudy/demo/controller/HelloController.java
+++ spring-example-study/src/main/java/com/limdongjin/springexamplestudy/demo/controller/HelloController.java
@@ -1,9 +1,13 @@
 package com.limdongjin.springexamplestudy.demo.controller;
 
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,4 +25,32 @@ public class HelloController {
 
         return "hello-mvc";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName("hello world!!"+name);
+
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
